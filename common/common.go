5a022d991481a37b42ea61e4acc420d788aa2317
Carlos Augusto Malucelli <camalucelli@gmail.com>
2020-06-02 16:48:35 -0300

    Move some codes :D

--- common/common.go
+++ common/common.go
@@ -1,15 +1,45 @@
 package common
 
-import "io/ioutil"
-
-const (
-	APIUrl = "https://kubernetes.default.svc/apis/metrics.k8s.io/v1beta1/namespaces/kube-system/pods"
-	// apiURL = "https://kubernetes.default.svc/apis/metrics.k8s.io/v1beta1/nodes"
-	Token  = "/var/run/secrets/kubernetes.io/serviceaccount/token"
-	CACert = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"net/http"
 )
 
-func ReturnDataFile(filePath string) []byte {
+const (
+	apiUrl = "https://kubernetes.default.svc/apis/metrics.k8s.io/v1beta1/namespaces/kube-system/pods"
+	// apiURL = "https://kubernetes.default.svc/apis/metrics.k8s.io/v1beta1/nodes"
+	token  = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+	caCert = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+)
+
+func ConnectToAPI() *http.Response {
+
+	var bearer = "Bearer " + string(returnDataFile(token))
+	req, err := http.NewRequest("GET", apiUrl, nil)
+	req.Header.Add("Authorization", bearer)
+
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(returnDataFile(caCert))
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs: caCertPool,
+			},
+		},
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic("Can't connect to api.")
+	}
+	return resp
+}
+
+func returnDataFile(filePath string) []byte {
+
 	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		panic(err)
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -16,38 +14,42 @@ import (
 
 func main() {
 
-	// Create a Bearer string by appending string access token
-	var bearer = "Bearer " + string(common.ReturnDataFile(common.Token))
+	// // Create a Bearer string by appending string access token
+	// var bearer = "Bearer " + string(common.ReturnDataFile(common.Token))
 
-	// Create a new request using http
-	req, err := http.NewRequest("GET", common.APIUrl, nil)
+	// // Create a new request using http
+	// req, err := http.NewRequest("GET", common.APIUrl, nil)
 
-	// add authorization header to the req
-	req.Header.Add("Authorization", bearer)
+	// // add authorization header to the req
+	// req.Header.Add("Authorization", bearer)
 
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(common.ReturnDataFile(common.CACert))
+	// caCertPool := x509.NewCertPool()
+	// caCertPool.AppendCertsFromPEM(common.ReturnDataFile(common.CACert))
 
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				RootCAs: caCertPool,
-			},
-		},
-	}
+	// client := &http.Client{
+	// 	Transport: &http.Transport{
+	// 		TLSClientConfig: &tls.Config{
+	// 			RootCAs: caCertPool,
+	// 		},
+	// 	},
+	// }
 
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println("Can't connect to api.\n[ERRO] -", err)
-	}
+	// resp, err := client.Do(req)
+	// if err != nil {
+	// 	log.Println("Can't connect to api.\n[ERRO] -", err)
+	// }
+
+	apiGo := common.ConnectToAPI()
 
 	// body, _ := ioutil.ReadAll(resp.Body)
 	// log.Println(string([]byte(body)))
 
 	var nodes node.Node
 	var pods pod.Pod
-	_ = json.NewDecoder(resp.Body).Decode(&nodes)
-	_ = json.NewDecoder(resp.Body).Decode(&pods)
+	// _ = json.NewDecoder(resp.Body).Decode(&nodes)
+	// _ = json.NewDecoder(resp.Body).Decode(&pods)
+	_ = json.NewDecoder(apiGo.Body).Decode(&nodes)
+	_ = json.NewDecoder(apiGo.Body).Decode(&pods)
 
 	log.Println(pods.Items[0].Metadata.Name, pods.Items[0].Containers[0].Name, pods.Items[0].Containers[0].Usage.CPU, pods.Items[0].Containers[0].Usage.Memory)
 	log.Println(nodes.Items[0].Metadata.Name, nodes.Items[0].Usage.CPU, nodes.Items[0].Usage.Memory)
