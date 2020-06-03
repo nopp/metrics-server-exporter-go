6a63c422accab6e1acd7bff20ee9ff09887fd115
Carlos Augusto Malucelli <camalucelli@gmail.com>
2020-06-03 10:17:52 -0300

    Fix

--- main.go
+++ main.go
@@ -1,25 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
 
-	"metrics-server-exporter-go/common"
-	"metrics-server-exporter-go/pod"
+	"metrics-server-exporter-go/config"
+	"metrics-server-exporter-go/prometheus"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 
-	apiGo := common.ConnectToAPI()
-
-	var pods pod.Info
-	_ = json.NewDecoder(apiGo.Body).Decode(&pods)
-
-	log.Println(pods)
+	prometheus.Collect()
 
 	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":8484", nil)
+	http.ListenAndServe(config.Host+":"+config.Port, nil)
 }
